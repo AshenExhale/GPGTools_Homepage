b69062982d54ea2273a7edd03b7e7202a363b462
Alexander Willner <alex@willner.ws>
2010-12-11 01:48:24 +0100

    be a bit verbose

--- build/compile.sh
+++ build/compile.sh
@@ -10,19 +10,23 @@ fi
 
 
 # php to html ------------------------------
+echo -n " * Running PHP...";
 for src in `ls *.php`; do
     dest=`basename -s .php "$src"`
     php $src > "$dest".html
 done
+echo "done.";
 # ------------------------------------------
 
 
 
 # .php links to .html links -----------------
+echo -n " * Changing '.php' to '.html'...";
 for html in `ls *.html`; do
     for php in `ls *.php`; do        
         filename=`basename -s .php "$php"`
         sed -i "" "s/$php/$filename.html/g" "$html"
     done
 done
+echo "done.";
 # ------------------------------------------
