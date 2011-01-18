fbc7847839200ee2b9f3b00ff0f81903337274cc
Alexander Willner <alex@willner.ws>
2011-01-18 21:10:36 +0100

    grammar

--- installer.php
+++ installer.php
@@ -16,7 +16,7 @@ $theSite->assign('lighthouse', '65162');
 
 $limitations = array(
 '36' => 'System wide installation not possible',
-'37' => 'Installer breaks GPG2 installations (already solved in the next <a href="http://github.com/GPGTools/GPGTools/downloads">current beta</a>)',
+'37' => 'Installer breaks GPG2 installations (already solved in the <a href="http://github.com/GPGTools/GPGTools/downloads">current beta</a>)',
 );
 $theSite->assign('limitations', $limitations);
 
