64bc81239c8838bc07dfb33840c8b0b99158e94c
Alexander Willner <alex@willner.ws>
2011-02-02 22:20:40 +0100

    added gpgmail appcast [#47]

---
+++ gpgmail_appcast.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"  xmlns:dc="http://purl.org/dc/elements/1.1/">
+<?php
+require('libs/Smarty.class.php');
+require('libs/Smarty.config.php');
+require('gpgmail.php.conf');
+$theSite->display('templates/appcast.tpl');
+?>
+</rss>
