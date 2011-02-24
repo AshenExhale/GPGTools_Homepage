75443609db8707a4c48e6c0018590408f1f5fd57
Roman Zechmeister <Roman.Zechmeister@aon.at>
2011-02-24 22:05:48 +0100

    PAD files for MacGPG1 and 2

---
+++ macgpg1_pad.php
@@ -0,0 +1,7 @@
+<?php
+/*fileExtension=xml*/
+require('libs/Smarty.class.php');
+require('libs/Smarty.config.php');
+require('macgpg1.php.conf');
+$theSite->display('templates/pad.tpl');
+?>
---
+++ macgpg2_pad.php
@@ -0,0 +1,7 @@
+<?php
+/*fileExtension=xml*/
+require('libs/Smarty.class.php');
+require('libs/Smarty.config.php');
+require('macgpg2.php.conf');
+$theSite->display('templates/pad.tpl');
+?>
