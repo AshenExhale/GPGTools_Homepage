9095c7606708bed1677b5f232e00aa800d80f772
Alexander Willner <alex@willner.ws>
2011-03-24 08:21:04 +0100

    version 1.4.11-5

--- macgpg1/config.php
+++ macgpg1/config.php
@@ -1,7 +1,7 @@
 <?php /*compileIgnore*/
 
-$version="1.4.11-1";
-$release['1.4.11-5'] = mktime(0, 1, 8, 03, 24, 2011);
+$version="1.4.11-5";
+$release['1.4.11-5'] = mktime(0, 8, 3, 03, 24, 2011);
 $release['1.4.11-1'] = mktime(0, 13, 8, 02, 24, 2011);
 $current = $release[$version];
 
@@ -11,7 +11,7 @@ $theSite->assign('img_logo', 'images/logo-128px.png');
 $theSite->assign('url_base', 'GPGTools/MacGPG1');
 $theSite->assign('url_dl',   "MacGPG1-$version.dmg");
 $theSite->assign('url_sig',  "MacGPG1-$version.dmg.sig");
-$theSite->assign('url_beta', 'MacGPG1-1.4.11-5.dmg');
+$theSite->assign('url_beta', '');
 $theSite->assign('url_wiki', '');
 $theSite->assign('version',  "$version");
 $theSite->assign('date',     date("d. F Y", $current));
@@ -37,6 +37,7 @@ $limitations = array(
 $theSite->assign('limitations', $limitations);
 
 $features = array(
+    "$root/images/universal.png" => array('Includes executable code for both PPC and Intel processors', 'PPC support'),
     "$root/images/installericon.png" => array('Is based on the GPGTools infrastructure and developments', 'Installer'),
 );
 $theSite->assign('features', $features);
@@ -51,12 +52,20 @@ $theSite->assign('screencasts', "");
 
 
 $changelog = array(
+    '1.4.11-5' => array(date("d. F Y", $release['1.4.11-5']), array(
+        'sparkle_date' => date(DATE_RFC2822, $release['1.4.11-5']),
+        'sparkle_url' => "http://github.com/downloads/GPGTools/MacGPG1/MacGPG1-1.4.11-5.dmg",
+        'sparkle_sig' => "",
+        'sparkle_size' => "3149997",
+        '2' => 'Added PPC support',
+        'sha' => "Checksum: 090fa41bc4c71a1d110a256d1d9623b5450576ee (SHA-1)",
+        )),
     '1.4.11-1' => array(date("d. F Y", $release['1.4.11-1']), array(
         'sparkle_date' => date(DATE_RFC2822, $release['1.4.11-1']),
         'sparkle_url' => "http://github.com/downloads/GPGTools/MacGPG1/MacGPG1-1.4.11-1.dmg",
         'sparkle_sig' => "",
         'sparkle_size' => "3077981",
-        'a' => 'initial version',
+        'a' => 'Initial version',
         'sha' => "Checksum: 908eb8b4a4a00fba92bc1f270419c10f2a4b3446 (SHA-1)",
         )),
 );
