99ed6b0eeef6225d437a4ecb050c730de46e6215
Alexander Willner <alex@willner.ws>
2011-03-29 14:49:41 +0200

    new build

--- libs/Smarty.config.php
+++ libs/Smarty.config.php
@@ -1,5 +1,5 @@
 <?php
-	date_default_timezone_set('Europe/Berlin'); // date_default_timezone issues on OS X
+    date_default_timezone_set('Europe/Berlin'); // date_default_timezone issues on OS X
 
         require('libs/Minify_HTML.class.php');
         function minifyHTML ($output, $smarty) {
@@ -7,10 +7,11 @@
             return $html->process();
         }
 
-	$theSite = new Smarty;
-	$theSite->assign('IDSITE', '1');
-	$theSite->assign('PIWIK_URL', 'www.gpgtools.org/piwik/');
-	$root = '.';
-	$theSite->assign('root', $root);
-        $theSite->registerFilter('output', 'minifyHTML');
+    $theSite = new Smarty;
+    $theSite->assign('IDSITE', '1');
+    $theSite->assign('PIWIK_URL', 'www.gpgtools.org/piwik/');
+    $theSite->assign('subtitle', '');
+    $root = '.';
+    $theSite->assign('root', $root);
+    $theSite->registerFilter('output', 'minifyHTML');
 ?>
--- libs/Smarty_subdir.config.php
+++ libs/Smarty_subdir.config.php
@@ -1,11 +1,19 @@
 <?php
-	date_default_timezone_set('Europe/Berlin'); // date_default_timezone issues on OS X
+    date_default_timezone_set('Europe/Berlin'); // date_default_timezone issues on OS X
 
-	$theSite = new Smarty;
-	$theSite->assign('IDSITE', '1');
-	$theSite->assign('PIWIK_URL', 'www.gpgtools.org/piwik/');
-	$root = '..';
-	$theSite->assign('root', $root);
-	$theSite->template_dir = "../templates/";
-	$theSite->compile_dir = "../templates_c/";
+        require('../libs/Minify_HTML.class.php');
+        function minifyHTML ($output, $smarty) {
+            $html = new Minify_HTML($output);
+            return $html->process();
+        }
+
+    $theSite = new Smarty;
+    $theSite->assign('IDSITE', '1');
+    $theSite->assign('PIWIK_URL', 'www.gpgtools.org/piwik/');
+    $theSite->assign('subtitle', '');
+    $root = '..';
+    $theSite->assign('root', $root);
+    $theSite->template_dir = "../templates/";
+    $theSite->compile_dir = "../templates_c/";
+    $theSite->registerFilter('output', 'minifyHTML');
 ?>
