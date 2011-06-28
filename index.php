23939858358582b19b1e5fed784621ac259751b9
Alexander Willner <alex@willner.ws>
2011-06-28 10:54:32 +0200

    prefetching everything related

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require('libs/Smarty.class.php');
 require('libs/Smarty.config.php');
 
 $theSite->assign('title', 'Official Homepage');
-$theSite->assign('prefetch', 'installer/index.html');
+//$theSite->assign('prefetch', 'installer/index.html');
 $theSite->display('templates/start.tpl');
 ?>
