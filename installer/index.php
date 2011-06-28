8133438a2f2051e44be680ec9e28242c62874485
Alexander Willner <alex@willner.ws>
2011-06-28 11:01:52 +0200

    prefetching everything related

--- installer/index.php
+++ installer/index.php
@@ -3,6 +3,6 @@ require('../libs/Smarty.class.php');
 require('../libs/Smarty_subdir.config.php');
 require('config.php');
 
-$theSite->assign('prefetch', 'projects.html');
+$theSite->assign('prefetch', '../projects.html');
 $theSite->display('project.tpl');
 ?>
